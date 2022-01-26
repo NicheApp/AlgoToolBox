b81e459d7fb2419af8bd0a699f387198e98df40a
Nishkarsh Maitry <47109963+NicheApp@users.noreply.github.com>
2022-01-27 01:07:33 +0530

    Create 3Sum.cpp

---
+++ ArrayinterviewQuestions/3Sum.cpp
@@ -0,0 +1,42 @@
+    vector<vector<int>> threeSum(vector<int>& nums) {
+     int n=nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        
+        for(unsigned int i=0;i<n;i++){
+            
+            int l= i+1; 
+            int r= n-1;
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            
+            while(l<r){
+                
+            int sum= nums[i]+nums[l]+nums[r];
+                
+                if(sum>0){
+                    
+                    r--;
+                }
+                else if(sum<0){
+                    l++;
+                    
+                }
+                else{
+                    ans.push_back(vector<int>{nums[i],nums[l],nums[r]});
+                    while(l<r && nums[l]==nums[l+1])l++;
+                    while(l< r && nums[r]==nums[r-1])r--;
+                    l++; r--;
+                    
+                }
+                
+                
+                
+            }
+            
+            
+        }
+        
+        return ans;
+        
+    }
